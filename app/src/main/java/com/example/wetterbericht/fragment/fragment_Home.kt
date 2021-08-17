a6989af78de6aef00e106ccbe27262fd206878c6
wayanalston185@gmail.com <iwayanalston87_>
2021-08-17 21:23:16 +0700

    Homeweather

--- app/src/main/java/com/example/wetterbericht/fragment/fragment_Home.kt
+++ app/src/main/java/com/example/wetterbericht/fragment/fragment_Home.kt
@@ -10,6 +10,8 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.wetterbericht.R
 import com.example.wetterbericht.fragment.adapter.todohomeadapter
+import com.example.wetterbericht.fragment.adapter.weatherhomeadapter
+import com.example.wetterbericht.viewmodel.room.Cuacaviewmodel
 import com.example.wetterbericht.viewmodel.room.todoviewmodel
 import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.android.synthetic.main.fragment_home.view.*
@@ -26,17 +28,28 @@ import kotlinx.android.synthetic.main.fragment_home.view.*
  */
 class fragment_Home : Fragment() {
     lateinit var mroomodel : todoviewmodel
+    lateinit var mcuacamodel : Cuacaviewmodel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_home, container, false)
 
+        //weather
+        val cadapter = weatherhomeadapter()
+        val mrecview = view.recviewweather
+        mrecview.adapter = cadapter
+        mrecview.layoutManager = LinearLayoutManager(requireContext())
+        mcuacamodel = ViewModelProvider(this).get(Cuacaviewmodel::class.java)
+        mcuacamodel.readdata.observe(viewLifecycleOwner, Observer { cuaca ->
+            cadapter.setdata(cuaca)
+        })
 
+
+        //to do list
         val madapter = todohomeadapter()
         val recyclerhome = view.recviewhome
         recyclerhome.adapter = madapter
         recyclerhome.layoutManager = LinearLayoutManager(requireContext())
-
         mroomodel = ViewModelProvider(this).get(todoviewmodel::class.java)
         mroomodel.readdata.observe(viewLifecycleOwner, Observer { data ->
             madapter.setdata(data)
